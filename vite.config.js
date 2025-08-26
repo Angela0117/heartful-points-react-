066e6029dd17c95668e38bf702dfd208f581ac97
Angela <angela.huang0117@gmail.com>
2025-08-26 23:50:39 +0800

    變更repo路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/heartful-points-react/' : '/',
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/heartful-points-react/' : '/'
+  
 })
